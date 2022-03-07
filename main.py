2d58e028c97ad40c52046a2a0739bd6ae4193af4
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-03-07 03:37:05 +0200

    stuck

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ class MenuWindow(Screen):
                 MenuWindow.once = True
                 return MenuWindow.once
 
-            if Command.sel is "shapes":
+            if Command.sel == "shapes":
                 ScreenManager().switch_to = "shapes"
-            elif Command.sel is "colours":
+            elif Command.sel == "colours":
                 ScreenManager().switch_to = "colours"
-            elif Command.sel is "letters":
+            elif Command.sel == "letters":
                 ScreenManager().switch_to = "letters"
-            elif Command.sel is "numbers":
+            elif Command.sel == "numbers":
                 ScreenManager().switch_to = "numbers"
             else:
                 pass
