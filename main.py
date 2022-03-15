2435e18a91b57d887c6b043f2bf048dc65e68c16
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-03-15 02:17:33 +0200

    progress was made today

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from itertools import count
 import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.image import Image
+from kivy.clock import Clock
 from kivy.properties import StringProperty
 from commands import *
 
@@ -12,56 +11,83 @@ class IntroWindow(Screen):
     pass
 
 class MenuWindow(Screen):
-    pass
-'''        once = False
-        def on_enter(self, *args):
+    once = False
+    def on_enter(self, *args):
+        while MenuWindow.once == False:
+            Command.start()
+            Command.introMenu()
             while MenuWindow.once == False:
-                Command.start()
-                Command.introMenu()
-                while MenuWindow.once == False:
-                    if Command.sel == "shapes":
-                        self.parent.current = "shapes"
-                        MenuWindow.once = True
-                    elif Command.sel == "colours":
-                        self.parent.current = "colours"
-                        MenuWindow.once = True
-                    elif Command.sel == "letters":
-                        self.parent.current = "letters"
-                        MenuWindow.once = True
-                    elif Command.sel == "numbers":
-                        self.parent.current = "numbers"
-                        MenuWindow.once = True
-                    else:
-                        MenuWindow.once = True
-                
+                if Command.sel == "shapes":
+                    self.parent.current = "shapes"
+                    MenuWindow.once = True
+                elif Command.sel == "colours":
+                    self.parent.current = "colours"
+                    MenuWindow.once = True
+                elif Command.sel == "letters":
+                    self.parent.current = "letters"
+                    MenuWindow.once = True
+                elif Command.sel == "numbers":
+                    self.parent.current = "numbers"
+                    MenuWindow.once = True
+                else:
+                    MenuWindow.once = True
+            
 
-            return MenuWindow.once
-'''
+        return MenuWindow.once
+
 
                 
         
 
 class ShapeGame(Screen):
     rand_shape = StringProperty()
-    count = 0
+    count1 = 0
+    count2 = 0
+
     def on_pre_enter(self, *args):        
-        random_shape = {"τετράγωνο":'shapes/square.png', "τρίγωνο":'shapes/triangle.jpg', "κύκλος":'shapes/circle.jpg'}
-        random_shape_key, random_shape_value = random.choice(list(random_shape.items()))
-        print(random_shape_key)
-        self.rand_shape_key = random_shape_key
-        self.rand_shape = random_shape_value
+        def callback_pre_image(dt):
+            if ShapeGame.count1 < 5:
+                if ShapeGame.count1 < 5:
+                    Command.say("Τι είναι?")
+                    random_shape = {"ορθογώνιο":'shapes/rectangle.jpg', "οβάλ":'shapes/oval.jpg', "αστέρι":'shapes/star.png', "τετράγωνο":'shapes/square.png', "τρίγωνο":'shapes/triangle.jpg', "κύκλος":'shapes/circle.jpg'}
+                    random_shape_key, random_shape_value = random.choice(list(random_shape.items()))
+                    print(random_shape_key)
+                    self.rand_shape_key = random_shape_key
+                    self.rand_shape = random_shape_value
+                    ShapeGame.count1 += 1
+                    return ShapeGame.count1
+                elif ShapeGame.count1 >= 5:
+                    pass
+            elif ShapeGame.count1 >=5:
+                Clock.unschedule(callback_pre_image)
+                ShapeGame.count1 = 0
+                return ShapeGame.count1
+
+        Clock.schedule_once(callback_pre_image)
+        Clock.schedule_interval(callback_pre_image, 10)
+
         
     def on_enter(self, *args):
-#        while self.count < 3:
-            input = Command.audioIn().lower().split(' ')
-            if any(word in input for word in [self.rand_shape_key]):
-                Command.say("Σωστά!")
-#                self.count += 1
-#                ShapeGame.on_pre_enter(self, *args)
-            else:
-                Command.say("Λάθος!")
-#                self.count += 1
-#                ShapeGame.on_pre_enter(self, *args)
+        def callback_on_image(dt):
+            if ShapeGame.count2 < 5:
+                if ShapeGame.count2 < 5:
+                    input = Command.audioIn().lower().split(' ')
+                    if any(word in input for word in [self.rand_shape_key]):
+                        Command.say("Σωστά!")
+                        ShapeGame.count2 += 1
+                        return ShapeGame.count2
+                    else:
+                        Command.say("Λάθος!")
+                        ShapeGame.count2 += 1
+                        return ShapeGame.count2
+            elif ShapeGame.count2 >=5:
+                Clock.unschedule(callback_on_image)
+                ShapeGame.count2 = 0
+                Command.say("Μπράβο!")
+                return ShapeGame.count2
+
+        Clock.schedule_once(callback_on_image)
+        Clock.schedule_interval(callback_on_image, 10)
 
     
 class ColourGame(Screen):
@@ -76,7 +102,9 @@ class NumberGame(Screen):
 class WindowManager(ScreenManager):
     pass
 
-class MainApp(App):  
+class MainApp(App):
+    
+
     def build(self):
         return Builder.load_file('Main.kv')
 
